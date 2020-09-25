3cf6868b13ecfc6be98b86fabed6b6836c5623d7
cascutter <cascutter13@gmail.com>
2020-09-25 16:11:26 -0500

    Created basic structure for class with property and method outlines

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+
+    // Method which returns name
+    getName() 
+
+    // Method which gets ID
+    getId()
+
+    //Method which gets email 
+    getEmail()
+
+    // Method which returns role (Employee)
+    getRole()
+}
+
+module.exports = Employee;
