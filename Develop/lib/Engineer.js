2a6246e6ca4d8c959bd30a12fae44cff9d27a829
cascutter <cascutter13@gmail.com>
2020-09-25 16:12:37 -0500

    Created basic structure inherited from Employee with class, properties, and method outlines

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(github) {
+        this.github = github;
+    }
+
+    // Method that returns GitHub username
+    getGithub()
+
+    // Method that overrides to return Engineer
+    getRole()
+}
