000f863002a2770cb4ed9036eb7e8ff68a057334
cascutter <cascutter13@gmail.com>
2020-09-29 15:15:24 -0500

    All prompts created and functional; if time permits, add validations

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,9 +11,121 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function createManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?", 
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is your office number?",
+        },
+    ]).then(function (answers) {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+        teamMembers.push(manager);
+        addTeamMember();
+    })
+}
+
+function addTeamMember() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "empType",
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add any more team members."
+            ]
+        }
+    ]).then(function (answer) {
+        if(answer.empType === "Engineer") {
+            createEngineer();
+        } else if(answer.empType === "Intern") {
+            createIntern();
+        } else {
+            console.log("Your team has been created!")
+            render(teamMembers)
+        }
+    })
+}
+
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is this engineer's ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is this engineer's email address?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is this engineer's GitHub?",
+        },
+    ]).then(function (answers) {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamMembers.push(engineer);
+        addTeamMember();
+    })
+}
+
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email address?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What school does the intern attend?",
+        },
+    ]).then(function (answers) {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamMembers.push(engineer);
+        addTeamMember();
+    })
+}
+
+createManager();
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
